ffcc3ab5e45aadcb91dad77ecc20d3cdacf8c259
Ashley Bland <ashleyjbland@gmail.com>
2018-08-21 22:35:04 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -60,10 +60,9 @@ def over?(board)
 end
 
 def winner(board)
-  won?(board).detect do |winner|
+  board[won?(board)].detect do |winner|
 
-    (position_1 == "X" && position_2 == "X" && position_3 == "X") ||
-      (position_1 == "O" && position_2 == "O" && position_3 == "O")
+    "X" || "O"
 
   end
 end
